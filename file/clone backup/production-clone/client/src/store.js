49e25d9d4c94e446cddc48b18c2dac98b59a0cea
Loganathan-Chandrasekar <loganathansamporanam@gmail.com>
2024-11-26 10:31:11 +0530

    Delete file/clone backup/production-clone directory

--- file/clone backup/production-clone/client/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers/index';
-
-const store = configureStore({
-  reducer: rootReducer,
-  // Add any middleware or other configuration options here
-});
-
-export default store;
--- file/clone backup/production-clone/server.js
+++
@@ -1,9 +0,0 @@
-import app from './index.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server Running On Port: ${port}`);
-});
